077e92b56b2e3d4077b28f12ab62d117f5002387
Arshdeep Singh <159508482+Arshdeep6840@users.noreply.github.com>
2026-06-04 11:33:24 +0530

    feat: initialize Django project structure with apps, configuration, and media support

--- codeguardian/codeguardian/settings.py
+++ codeguardian/codeguardian/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'scanner',
+    'dashboard',
+    'fixes',
+    'issues',
+    'reports',
+    'home'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +128,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- codeguardian/codeguardian/urls.py
+++ codeguardian/codeguardian/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('accounts.urls')),
+    path('', include('scanner.urls')),
+    path('', include('issues.urls')),
+    path('', include('fixes.urls')),
+    path('', include('dashboard.urls')),    
+    path('', include('reports.urls')),
+    path('', include('home.urls')),
+
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
