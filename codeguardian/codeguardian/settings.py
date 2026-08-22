6a8818b70cc6231eeff487025c3faa37f6de1828
Arshdeep Singh <159508482+Arshdeep6840@users.noreply.github.com>
2026-08-22 10:58:39 +0530

    feat: implement landing page and project dashboard UI templates with static assets

--- codeguardian/codeguardian/settings.py
+++ codeguardian/codeguardian/settings.py
@@ -156,11 +156,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-]
-
-CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
 ]
+
 
 CORS_ALLOW_ALL_ORIGINS = True # remove on production
 # AUTH_USER_MODEL = "accounts.User"
